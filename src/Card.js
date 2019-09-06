7d5df991b0bea4421b4b901ebd67997ed6608a42
DrDeer <wasi.labrador@gmail.com>
2019-09-06 11:13:31 +0800

    single robots

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Card = () =>{
 	return(
 		<div>
-		<img alt='robots' src=''/>
+		<img alt='robots' src='https://robohash.org/test?2000*20000'/>
 			<div>
 				<h2>Jane Doe</h2>
 				<p>Jane.doe@gmail.com</p>
