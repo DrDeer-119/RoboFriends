0cee63e3a661c0c5590e748844e0e8a710f12847
DrDeer <wasi.labrador@gmail.com>
2019-09-21 01:07:33 +0800

    194 State Activate SearchBox finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React,{Component} from 'react';
 import CardList from './cardList';
-import SerachBox from './SearchBox';
+import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-const App = () => {
-	return(
-			<div>
-				<h1>RoboFriends</h1>
-				<SerachBox />
-				<CardList robots = {robots}/>
-			</div>
-		);
+
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots:robots,
+			searchfield:''
+		}
+	}
+
+
+
+	onSearchCange = (event) => {
+		this.setState({searchfield:event.target.value})
+		}
+	
+
+	render(){
+				// 下一行filter是陣列的方法
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+
+		
+
+		return(
+
+				<div className='tc'>
+					<h1>RoboFriends</h1>
+					<SearchBox searchChange = {this.onSearchCange}/>
+					<CardList robots = {filteredRobots}/>
+				</div>
+			);
+	}
 }
 
 export default App;
